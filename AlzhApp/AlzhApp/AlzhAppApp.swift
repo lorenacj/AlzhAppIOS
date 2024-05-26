305c90f10ee32d056dd47f6c497d0597dc31fd37
Lorena cruz <lorena.cruz@alten.es>
2024-05-26 23:30:57 +0200

    main - started project

---
+++ AlzhApp/AlzhApp/AlzhAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlzhAppApp.swift
+//  AlzhApp
+//
+//  Created by lorena.cruz on 26/5/24.
+//
+
+import SwiftUI
+
+@main
+struct AlzhAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlzhApp/AlzhApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlzhApp
+//
+//  Created by lorena.cruz on 26/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
