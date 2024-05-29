d477bf4827e098665ef2dff75e1bcf9cd7fbd9df
Lorena cruz <lorena.cruz@alten.es>
2024-05-29 20:18:40 +0200

    develop - added fonts, strings localizables, appfonts

---
+++ AlzhApp/resources/AppFonts.swift
@@ -0,0 +1,45 @@
+//
+//  AppFonts.swift
+//  AlzhApp
+//
+//  Created by lorena.cruz on 29/5/24.
+//
+
+import Foundation
+
+// MARK: - AppFonts
+enum AppFonts {
+    enum SourceSans: String {
+        case black = "SourceSans3-Black"
+        case blackItalic = "SourceSans3-BlackItalic"
+        case bold = "SourceSans3-Bold"
+        case boldItalic = "SourceSans3-BoldItalic"
+        case extraBold = "SourceSans3-ExtraBold"
+        case extraBoldItalic = "SourceSans3-ExtraBoldItalic"
+        case extraLight = "SourceSans3-ExtraLight"
+        case extraLightItalic = "SourceSans3-ExtraLightItalic"
+        case italic = "SourceSans3-Italic"
+        case light = "SourceSans3-Light"
+        case lightItalic = "SourceSans3-LightItalic"
+        case medium = "SourceSans3-Medium"
+        case mediumItalic = "SourceSans3-MediumItalic"
+        case regular = "SourceSans3-Regular"
+        case semibold = "SourceSans3-SemiBold"
+        case semiBoldItalic = "SourceSans3-SemiBoldItalic"
+    }
+    
+    enum OpenSans: String {
+        case bold = "OpenSans-Bold"
+        case boldItalic = "OpenSans-BoldItalic"
+        case extraBold = "OpenSans-ExtraBold"
+        case extraBoldItalic = "OpenSans-ExtraBoldItalic"
+        case italic = "OpenSans-Italic"
+        case light = "OpenSans-Light"
+        case lightItalic = "OpenSans-LightItalic"
+        case medium = "OpenSans-Medium"
+        case mediumItalic = "OpenSans-MediumItalic"
+        case regular = "OpenSans-Regular"
+        case semibold = "OpenSans-SemiBold"
+        case semiBoldItalic = "OpenSans-SemiBoldItalic"
+    }
+}
