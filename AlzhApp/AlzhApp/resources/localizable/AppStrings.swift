3a4993bd8fd76551d895e13fd11d9f4a56161371
Lorena cruz <lorena.cruz@alten.es>
2024-05-30 12:27:47 +0200

    develop - changed text to localizable strings + button
    Incorporated button functionality. The implementation is nearly complete. Once the API is ready, verify the username and password. If correct, navigate to the initial view.

--- AlzhApp/AlzhApp/resources/localizable/AppStrings.swift
+++ AlzhApp/AlzhApp/resources/localizable/AppStrings.swift
@@ -9,5 +9,15 @@ import Foundation
 
 // MARK: - LocalizedString
 struct LocalizedString {
-    static let titleForgotPassword = NSLocalizedString("titulo_forgotpassword", comment: "")
+    static let alzhapp = NSLocalizedString("AlzhApp", comment: "")
+    static let login = NSLocalizedString("login", comment: "")
+    static let validacion = NSLocalizedString("validacion", comment: "")
+    static let dni = NSLocalizedString("dni", comment: "")
+    static let dniplaceholder = NSLocalizedString("dni_placeholder", comment: "")
+    static let password = NSLocalizedString("password", comment: "")
+    static let placeholderGeneral = NSLocalizedString("placeholder_general", comment: "")
+    static let noTieneCuenta = NSLocalizedString("no_tiene_cuenta", comment: "")
+    static let camposVacios = NSLocalizedString("campos_vacios", comment: "")
+    static let loginCorrecto = NSLocalizedString("login_correcto", comment: "")
+    static let okbutton = NSLocalizedString("ok", comment: "")
 }
--- AlzhApp/AlzhApp/views/login/LoginView.swift
+++ AlzhApp/AlzhApp/views/login/LoginView.swift
@@ -11,6 +11,8 @@ struct LoginView: View {
     @State private var dniText: String = ""
     @State private var passwordText: String = ""
     @State private var isTapped = false
+    @State private var showAlert = false
+    @State private var alertMessage = ""
 
     var body: some View {
         GeometryReader { proxy in
@@ -20,27 +22,38 @@ struct LoginView: View {
                     Image("logo")
                         .resizable()
                         .frame(width: 150, height: 150)
-                    Text("AlzhApp")
+                    Text(LocalizedString.alzhapp)
                         .font(.custom(AppFonts.OpenSans.regular.rawValue, size: 40))
                         .foregroundStyle(.white)
                         .bold()
                         .shadow(color: .black, radius: 3, x: 0, y: 0)
                         .padding(.bottom, 20)
-                    CustomTextFieldAuth(title: "DNI", placeholder: "123456789X", text: $dniText, isSecureField: false)
+                    CustomTextFieldAuth(title: LocalizedString.dni, placeholder: LocalizedString.dniplaceholder, text: $dniText, isSecureField: false)
                         .padding(.horizontal, 40)
                         .frame(maxWidth: .infinity)
-                    CustomTextFieldAuth(title: "PASSWORD", placeholder: "Write here", text: $passwordText, isSecureField: true)
+                    CustomTextFieldAuth(title: LocalizedString.password, placeholder: "Write here", text: $passwordText, isSecureField: true)
                         .padding(.horizontal, 40)
                         .frame(maxWidth: .infinity)
-                    Text("¿Has olvidado tu contraseña?")
+                    Text(LocalizedString.noTieneCuenta)
                         .font(.system(size: 12))
                         .foregroundColor(AppColors.darkBlue)
                         .opacity(1)
                         .frame(maxWidth: .infinity, alignment: .trailing)
-                        .padding(.trailing, 30)
+                        .padding(.trailing, 40)
                     Spacer()
-                    CustomButtonStyle(text: "Login", isTapped: $isTapped) {
-                        
+                    CustomButtonStyle(text: LocalizedString.login, isTapped: $isTapped) {
+                        if dniText.isEmpty || passwordText.isEmpty {
+                            alertMessage = LocalizedString.camposVacios
+                        } else {
+                            #warning("Comprobar si es correcto o no")
+                            //si es correcto:
+                            alertMessage = LocalizedString.loginCorrecto
+                            dniText = ""
+                            passwordText = ""
+                        }
+                        showAlert = true
+                    }.alert(isPresented: $showAlert) {
+                        Alert(title: Text(LocalizedString.validacion), message: Text(alertMessage), dismissButton: .default(Text(LocalizedString.okbutton)))
                     }
                     .padding(.horizontal, 40)
                     .padding(.bottom, 20)
